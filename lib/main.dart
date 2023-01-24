36660e8ed272015e3caff780ca4a0786a750a2f7
sev-n <nielsevilla39@gmail.com>
2023-01-24 20:45:11 +0800

    Changed page indicator

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/material.dart';
-import '/welcome_page.dart';
+import 'welcome_page.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  const MyApp({super.key});
 
   // This widget is the root of your application.
   @override
--- lib/welcome_page.dart
+++ lib/welcome_page.dart
@@ -2,14 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 
 class WelcomePage extends StatefulWidget {
-  const WelcomePage({Key? key}) : super(key: key);
+  const WelcomePage({super.key});
 
   @override
   State<WelcomePage> createState() => _WelcomePageState();
 }
 
 class _WelcomePageState extends State<WelcomePage> {
-  PageController _controller = PageController();
+  final PageController _controller = PageController();
 
   @override
   Widget build(BuildContext context) {
@@ -18,14 +18,37 @@ class _WelcomePageState extends State<WelcomePage> {
         PageView(
           controller: _controller,
           children: [
-            Container(color: Colors.blue),
-            Container(color: Colors.red),
-            Container(color: Colors.green),
+            Container(
+              color: Colors.blueGrey,
+              child: const Center(
+                child: Text("First page"),
+              ),
+            ),
+            Container(
+              color: Colors.blue,
+              child: const Center(
+                child: Text("Second page"),
+              ),
+            ),
+            Container(
+              color: Colors.orangeAccent,
+              child: const Center(
+                child: Text("Third page"),
+              ),
+            ),
           ],
         ),
         Container(
           alignment: const Alignment(0, 0.80),
-          child: SmoothPageIndicator(controller: _controller, count: 3)),
+          child: SmoothPageIndicator(
+            controller: _controller,
+            count: 3,
+            effect: const ScrollingDotsEffect(
+                activeDotScale: 0.5,
+                radius: 8,
+                spacing: 10),
+          ),
+        ),
       ]),
     );
   }
