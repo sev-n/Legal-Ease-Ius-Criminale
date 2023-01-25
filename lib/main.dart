0369d8016d779a3e046edf0224cd2cc81ec71b4d
sev-n <nielsevilla39@gmail.com>
2023-01-25 19:20:22 +0800

    Added button & home page

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'welcome_page.dart';
+import 'src/welcome_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Flutter App',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
--- lib/src/welcome_page.dart
+++ lib/src/welcome_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:legalease_matrimonial/src/home.dart';
 import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 
 class WelcomePage extends StatefulWidget {
@@ -10,14 +11,42 @@ class WelcomePage extends StatefulWidget {
 
 class _WelcomePageState extends State<WelcomePage> {
   final PageController _controller = PageController();
+  bool isLastPage = false;
+
+  Widget getStarted() {
+    return Container(
+      alignment: const Alignment(0, 0.60),
+      child: ElevatedButton(
+        onPressed: () {
+          Navigator.push(context, MaterialPageRoute(builder: (context) {
+            return const Home();
+          }));
+        },
+        style: ElevatedButton.styleFrom(
+          backgroundColor: Colors.white,
+          foregroundColor: Colors.black,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(32.0),
+          ),
+        ),
+        child: const Text("Get Started"),
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Stack(children: [
         PageView(
+          onPageChanged: (index) {
+            setState(() {
+              isLastPage = (index == 2);
+            });
+          },
           controller: _controller,
           children: [
+            // these children need to seperate for better reading
             Container(
               color: Colors.blueGrey,
               child: const Center(
@@ -38,15 +67,14 @@ class _WelcomePageState extends State<WelcomePage> {
             ),
           ],
         ),
+        if (isLastPage) getStarted(),
         Container(
           alignment: const Alignment(0, 0.80),
           child: SmoothPageIndicator(
             controller: _controller,
             count: 3,
             effect: const ScrollingDotsEffect(
-                activeDotScale: 0.5,
-                radius: 8,
-                spacing: 10),
+                activeDotScale: 0.5, radius: 6, spacing: 11),
           ),
         ),
       ]),
