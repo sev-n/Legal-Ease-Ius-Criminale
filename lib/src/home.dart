0cce0626997b00fed8894db46e0a204df6f69a02
Jchael12 <johnmichaelabbas23@gmail.com>
2023-01-27 01:54:01 +0800

    homepage and sidenav

--- lib/src/home.dart
+++ lib/src/home.dart
@@ -12,165 +12,137 @@ class _HomeState extends State<Home> {
   Widget build(BuildContext context) {
     return DefaultTabController(
       length: 4,
-      child: Scaffold(
-        appBar: AppBar(
-          backgroundColor: const Color(0xff272f31),
-          title: const Text(
-            'LegalEase',
-            style: TextStyle(fontWeight: FontWeight.bold),
+      child: Container(
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage("images/LegalEase_BG.png"),
+            fit: BoxFit.fill,
           ),
-          leading: Builder(builder: (BuildContext context) {
-            return IconButton(
-              icon: const Icon(Icons.grid_view_rounded),
-              onPressed: () {
-                Scaffold.of(context).openDrawer();
-              },
-              tooltip: MaterialLocalizations.of(context).openAppDrawerTooltip,
-            );
-          }),
-          actions: [
-            IconButton(
-              onPressed: () {
-                showSearch(
-                  context: context,
-                  delegate: CustomSearchDelegate(),
-                );
-              },
-              icon: const Icon(Icons.search),
+        ),
+        child: Scaffold(
+          backgroundColor: Colors.transparent,
+          extendBodyBehindAppBar: true,
+          //Side Navigation Menu code
+          drawer: Drawer(
+            backgroundColor: const Color(0xff4f555d),
+            child: ListView(
+              padding: EdgeInsets.zero,
+              children: <Widget>[
+                const DrawerHeader(
+                  decoration: BoxDecoration(
+                    color: Color(0xff272f31),
+                  ),
+                  child: Text(
+                    'Legal Ease',
+                    style: TextStyle(color: Colors.white, fontSize: 24),
+                  ),
+                ),
+                Column(
+                  children: const [
+                    ListTile(
+                        leading: Icon(Icons.menu_book_outlined),
+                        title: Text(
+                          'Articulos',
+                          style: TextStyle(color: Colors.white),
+                        ),
+                        iconColor: Colors.white),
+                    ListTile(
+                      leading: Icon(Icons.bookmark_rounded),
+                      title: Text(
+                        'Bookmark',
+                        style: TextStyle(color: Colors.white),
+                      ),
+                      iconColor: Colors.white,
+                    ),
+                    ListTile(
+                      leading: Icon(Icons.settings_applications_rounded),
+                      title: Text(
+                        'Settings',
+                        style: TextStyle(color: Colors.white),
+                      ),
+                      iconColor: Colors.white,
+                    ),
+                    ListTile(
+                      leading: Icon(Icons.feedback_rounded),
+                      title: Text(
+                        'Feedback',
+                        style: TextStyle(color: Colors.white),
+                      ),
+                      iconColor: Colors.white,
+                    ),
+                    ListTile(
+                      leading: Icon(Icons.help_center_rounded),
+                      title: Text(
+                        'Help',
+                        style: TextStyle(color: Colors.white),
+                      ),
+                      iconColor: Colors.white,
+                    ),
+                  ],
+                ),
+              ],
             ),
-          ],
-          bottom: const TabBar(
-            tabs: [
-              Tab(
-                icon: Icon(Icons.menu_book_rounded),
-                text: 'Abbas',
-              ),
-              Tab(
-                icon: Icon(Icons.menu_book_rounded),
-                text: 'Lurion',
-              ),
-              Tab(
-                icon: Icon(Icons.menu_book_rounded),
-                text: 'Panis',
-              ),
-              Tab(
-                icon: Icon(Icons.menu_book_rounded),
-                text: 'Khane',
-              ),
-            ],
+          ),
+          body: SafeArea(
+            child: Column(
+              children: [
+                AppBar(
+                  elevation: 0,
+                  bottomOpacity: 0,
+                  shadowColor: Colors.transparent,
+                  backgroundColor: Colors.transparent,
+                  title: const Text(
+                    'LegalEase',
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                  leading: Builder(
+                    builder: (BuildContext context) {
+                      return IconButton(
+                        icon: const Icon(Icons.grid_view_rounded),
+                        onPressed: () {
+                          Scaffold.of(context).openDrawer();
+                        },
+                        tooltip: MaterialLocalizations.of(context)
+                            .openAppDrawerTooltip,
+                      );
+                    },
+                  ),
+                ),
+                const Divider(
+                  color: Color(0xff93979a),
+                  thickness: 3,
+                ),
+                //Tabbar code
+                const TabBar(
+                  indicatorColor: Color(0xff93979a),
+                  tabs: [
+                    Tab(
+                      icon: Icon(Icons.menu_book_rounded),
+                      text: 'Jhon',
+                    ),
+                    Tab(
+                      icon: Icon(Icons.menu_book_rounded),
+                      text: 'Lurion',
+                    ),
+                    Tab(
+                      icon: Icon(Icons.menu_book_rounded),
+                      text: 'Panis',
+                    ),
+                    Tab(
+                      icon: Icon(Icons.menu_book_rounded),
+                      text: 'Khane',
+                    ),
+                  ],
+                ),
+                Container(
+                  alignment: Alignment.center,
+                  padding: const EdgeInsets.all(32),
+                ),
+              ],
+            ),
           ),
         ),
-        drawer: Drawer(
-          child: ListView(
-            padding: EdgeInsets.zero,
-            children: const <Widget>[
-              DrawerHeader(
-                decoration: BoxDecoration(
-                  color: Color(0xff272f31),
-                ),
-                child: Text(
-                  'Legal Ease',
-                  style: TextStyle(color: Colors.white, fontSize: 24),
-                ),
-              ),
-              ListTile(
-                leading: Icon(Icons.menu_book_outlined),
-                title: Text('Articulos'),
-              ),
-              ListTile(
-                leading: Icon(Icons.bookmark_rounded),
-                title: Text('Bookmark'),
-              ),
-              ListTile(
-                leading: Icon(Icons.settings_applications_rounded),
-                title: Text('Settings'),
-              ),
-              ListTile(
-                leading: Icon(Icons.feedback_rounded),
-                title: Text('Feedback'),
-              ),
-              ListTile(
-                leading: Icon(Icons.help_center_rounded),
-                title: Text('Help'),
-              ),
-            ],
-          ),
-        ),
-        body: const Center(
-          child: Text("Start here!"),
-        ),
       ),
-    );
-  }
-}
-
-// Search Bar code
-class CustomSearchDelegate extends SearchDelegate {
-  List<String> searchTerms = [
-    'Apples',
-    'Banana',
-    'Pear',
-    'Orange',
-    'Strawberry',
-  ];
-
-  @override
-  List<Widget> buildActions(BuildContext context) {
-    return [
-      IconButton(
-        onPressed: () {
-          query = '';
-        },
-        icon: const Icon(Icons.clear),
-      ),
-    ];
-  }
-
-  @override
-  Widget buildLeading(BuildContext context) {
-    return IconButton(
-      icon: const Icon(Icons.arrow_back),
-      onPressed: () {
-        close(context, null);
-      },
-    );
-  }
-
-  @override
-  Widget buildResults(BuildContext context) {
-    List<String> matchQuery = [];
-    for (var fruit in searchTerms) {
-      if (fruit.toLowerCase().contains(query.toLowerCase())) {
-        matchQuery.add(fruit);
-      }
-    }
-    return ListView.builder(
-      itemCount: matchQuery.length,
-      itemBuilder: (context, index) {
-        var result = matchQuery[index];
-        return ListTile(
-          title: Text(result),
-        );
-      },
-    );
-  }
-
-  @override
-  Widget buildSuggestions(BuildContext context) {
-    List<String> matchQuery = [];
-    for (var fruit in searchTerms) {
-      if (fruit.toLowerCase().contains(query.toLowerCase())) {
-        matchQuery.add(fruit);
-      }
-    }
-    return ListView.builder(
-      itemCount: matchQuery.length,
-      itemBuilder: (context, index) {
-        var result = matchQuery[index];
-        return ListTile(
-          title: Text(result),
-        );
-      },
     );
   }
 }
