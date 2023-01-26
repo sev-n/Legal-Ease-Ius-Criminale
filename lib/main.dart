81fc0b6ccfc8fecfbbb516e3c736f880cc4363e3
Nerwinn <panis.nerwin.eccbscs@gmail.com>
2023-01-26 12:22:19 +0800

    manok version 2

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'manok',
+      title: 'manok version 2',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
