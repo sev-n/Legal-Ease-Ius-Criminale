826da8bdad2eecf710831047f48b7621e4a68f1c
sev-n <nielsevilla39@gmail.com>
2023-02-05 20:34:50 +0800

    Added const

--- lib/src/article_page.dart
+++ lib/src/article_page.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 
 class ArticlePage extends StatelessWidget {
+  const ArticlePage({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
--- lib/src/bookmark_page.dart
+++ lib/src/bookmark_page.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 
 class BookmarkPage extends StatelessWidget {
+  const BookmarkPage({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
