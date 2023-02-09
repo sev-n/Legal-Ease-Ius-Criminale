f200683b398a4989fdc67b688e25c44de4584bd3
sev-n <nielsevilla39@gmail.com>
2023-02-09 20:54:25 +0800

    resolved issue

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -37,6 +37,7 @@ class HomePage extends StatelessWidget {
     return DefaultTabController(
       length: tabsList.length,
       child: SafeArea(
+        bottom: true,
         child: Scaffold(
           resizeToAvoidBottomInset: false,
           body: Align(
@@ -63,110 +64,48 @@ class HomePage extends StatelessWidget {
                           width: MediaQuery.of(context).size.width * 0.2,
                           height: MediaQuery.of(context).size.height * 0.2),
                     ),
-                    Positioned(
-                      top: 120,
-                      left: 0,
-                      right: 0,
-                      child: SingleChildScrollView(
-                        scrollDirection: Axis.horizontal,
-                        child: TabBar(
-                            indicatorColor: const Color(0xCCD9D9D9),
-                            isScrollable: true,
-                            tabs: tabsList),
+                    const Laws(),
+                    const Positioned(
+                      top: 175,
+                      left: 20,
+                      child: Text(
+                        "Consultation",
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold, fontSize: 20),
                       ),
+                    ),
+                    Container(
+                      alignment: const Alignment(0.0, -0.40),
+                      child: const _TextField(),
+                    ),
+                    const Positioned(
+                      top: 280,
+                      right: 138,
+                      child: GenerateBtn(),
                     ),
                   ],
                 ),
               ),
-              
-              // CUSTOMIZE TAB BAR CODE
-              Positioned(
-                top: 80,
-                left: 22,
-                right: 10,
-                child: Center(
-                  child: Row(
-                    children: <Widget>[
-                      Column(
-                        children: const [
-                          tabBox(),
-                          SizedBox(height: 8,),
-                          Text("Traffic \nOffense", style: TextStyle(color: Colors.white),)
-                        ],
-                      ),
-                      const SizedBox(width: 10,),
-                      Column(
-                        children: const [
-                          tabBox(),
-                          SizedBox(height: 8,),
-                          Text("Against \nPerson", style: TextStyle(color: Colors.white),)
-                        ],
-                      ),
-                      const SizedBox(width: 10,),
-                      Column(
-                        children: const [
-                          tabBox(),
-                          SizedBox(height: 8,),
-                          Text("Against \nProperty", style: TextStyle(color: Colors.white),)
-                        ],
-                      ), 
-                      const SizedBox(width: 10,),
-                      Column(
-                        children: const [
-                          tabBox(),
-                          SizedBox(height: 8,),
-                          Text("Statutory \nRights", style: TextStyle(color: Colors.white),)
-                        ],
-                      ), 
-                      const SizedBox(width: 10,),
-                      Column(
-                        children: const [
-                          tabBox(),
-                          SizedBox(height: 8,),
-                          Text("White \nCollar", style: TextStyle(color: Colors.white),)
-                        ],
-                      ), 
-                      const SizedBox(width: 10,),
-                      Column(
-                        children: const [
-                          tabBox(),
-                          SizedBox(height: 8,),
-                          Text("Inchoate \nCrimes", style: TextStyle(color: Colors.white),)
-                        ],
-                      )
-                    ],
-                  ),
-                ),
-              ),
-              // CUSTOMIZE TAB BAR CODE
 
+              // Body part
               Align(
                 alignment: Alignment.bottomCenter,
                 child: SizedBox(
-                  width: MediaQuery.of(context).size.width,
-                  height: MediaQuery.of(context).size.height - 380,
+                  width: MediaQuery.of(context).size.width - 25,
+                  height: MediaQuery.of(context).size.height - 420,
                   child: Container(
                     decoration: const BoxDecoration(
-                      color: Color(0xCCD9D9D9),
+                      color: Color(0x99D9D9D9),
                       borderRadius: BorderRadius.only(
-                        topLeft: Radius.circular(10),
-                        topRight: Radius.circular(10),
+                        topLeft: Radius.circular(15),
+                        topRight: Radius.circular(15),
                       ),
                     ),
                     // !alert: insert items here!
-                    child: const Align(
-                      alignment: Alignment.topCenter,
-                      child: _TextField(),
-                    ),
+                    child: null,
                   ),
                 ),
               ),
-
-
-              Container(
-                alignment: const Alignment(0.0, -0.30),
-                child: const _TextField()
-              )
             ]),
           ),
         ),
@@ -188,61 +127,152 @@ class _TextField extends StatefulWidget {
 class __TextFieldState extends State<_TextField> {
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: const [
-              Padding(
-                padding: EdgeInsets.only(left: 12),
-                child: Text("Consultation", style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),),
-              ),
-            ],
+    return const SingleChildScrollView(
+      child: Padding(
+        padding: EdgeInsets.all(20.0),
+        child: TextField(
+          minLines: 2,
+          maxLines: 2,
+          keyboardType: TextInputType.multiline,
+          decoration: InputDecoration(
+            filled: true,
+            fillColor: Color(0xffD9D9D9),
+            hintText: "test",
+            hintStyle: TextStyle(color: Colors.grey),
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.all(Radius.circular(15.0)),
+            ),
           ),
-          Padding(
-            padding: const EdgeInsets.all(10.0),
-            // ignore: sized_box_for_whitespace
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                // ignore: sized_box_for_whitespace
-                Container(
-                  width: double.infinity,
-                  child: const TextField(
-                    minLines: 2,
-                    maxLines: 2,
-                    keyboardType: TextInputType.multiline,
-                    decoration: InputDecoration(
-                      filled: true,
-                      fillColor: Colors.white,
-                      hintText: "test",
-                      hintStyle: TextStyle(color: Colors.grey),
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.all(Radius.circular(20.0)),
-                      ),
-                    ),
-                  ),
+        ),
+      ),
+    );
+  }
+}
+
+class GenerateBtn extends StatelessWidget {
+  const GenerateBtn({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      style: ButtonStyle(
+          alignment: Alignment.centerRight,
+          backgroundColor: MaterialStateProperty.all(const Color(0xffD9D9D9)),
+          shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+              RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10.0),
+                  side: const BorderSide(color: Color(0xff97a294))))),
+      onPressed: () {},
+      child: const Text(
+        "Generate",
+        style: TextStyle(color: Color(0xCC000000)),
+      ),
+    );
+  }
+}
+
+class Laws extends StatelessWidget {
+  const Laws({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Positioned(
+      top: 80,
+      left: 25.5,
+      //right: 10,
+      child: Center(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: <Widget>[
+            Column(
+              children: const [
+                tabBox(),
+                SizedBox(
+                  height: 8,
                 ),
-                const SizedBox(height: 5.0,),
-                ElevatedButton(
-                  style: ButtonStyle(
-                    alignment: Alignment.centerRight,
-                    backgroundColor: MaterialStateProperty.all<Color>(
-                    const Color(0xCCD9D9D9)),
-                    shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                      RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(10.0),
-                        side: const BorderSide(
-                        color: Color(0xCCD9D9D9))))),
-                    onPressed: () {},
-                    child: const Text("Generate"),
+                Text(
+                  "Traffic \nOffense",
+                  style: TextStyle(color: Colors.white, fontSize: 10),
                 )
               ],
             ),
-          ),
-        ],
+            const SizedBox(
+              width: 10,
+            ),
+            Column(
+              children: const [
+                tabBox(),
+                SizedBox(
+                  height: 8,
+                ),
+                Text(
+                  "Against \nPerson",
+                  style: TextStyle(color: Colors.white, fontSize: 10),
+                )
+              ],
+            ),
+            const SizedBox(
+              width: 10,
+            ),
+            Column(
+              children: const [
+                tabBox(),
+                SizedBox(
+                  height: 8,
+                ),
+                Text(
+                  "Against \nProperty",
+                  style: TextStyle(color: Colors.white, fontSize: 10),
+                )
+              ],
+            ),
+            const SizedBox(
+              width: 10,
+            ),
+            Column(
+              children: const [
+                tabBox(),
+                SizedBox(
+                  height: 8,
+                ),
+                Text(
+                  "Statutory \nRights",
+                  style: TextStyle(color: Colors.white, fontSize: 10),
+                )
+              ],
+            ),
+            const SizedBox(
+              width: 10,
+            ),
+            Column(
+              children: const [
+                tabBox(),
+                SizedBox(
+                  height: 8,
+                ),
+                Text(
+                  "White \nCollar",
+                  style: TextStyle(color: Colors.white, fontSize: 10),
+                )
+              ],
+            ),
+            const SizedBox(
+              width: 10,
+            ),
+            Column(
+              children: const [
+                tabBox(),
+                SizedBox(
+                  height: 8,
+                ),
+                Text(
+                  "Inchoate \nCrimes",
+                  style: TextStyle(color: Colors.white, fontSize: 10),
+                )
+              ],
+            )
+          ],
+        ),
       ),
     );
   }
