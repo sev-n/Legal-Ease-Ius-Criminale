a1c15d59edae92d47c6596b1945ea413b3eedbd6
sev-n <nielsevilla39@gmail.com>
2023-02-13 12:23:28 +0800

    Fixed keyboard auto close when unfocus

--- lib/src/home_option.dart
+++ lib/src/home_option.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:legalease_matrimonial/src/sidenav.dart';
 import 'pages/contents/content.dart';
 
+TextEditingController _textEditingController = TextEditingController();
+
 class HomeOption extends StatelessWidget {
   const HomeOption({super.key});
 
@@ -9,6 +11,7 @@ class HomeOption extends StatelessWidget {
   Widget build(BuildContext context) {
     double screenHeight = MediaQuery.of(context).size.height;
     double screenWidth = MediaQuery.of(context).size.width;
+    FocusScopeNode textFieldFocus = FocusScope.of(context);
 
     return Scaffold(
       resizeToAvoidBottomInset: false,
@@ -17,168 +20,188 @@ class HomeOption extends StatelessWidget {
           alignment: Alignment.center,
           child: Stack(
             children: [
-              Container(
-                  height: screenHeight,
-                  width: screenWidth,
-                  decoration: const BoxDecoration(
-                    image: DecorationImage(
-                        image: AssetImage('assets/image_option/OPTIONAL.png'),
-                        fit: BoxFit.cover),
-                  ),
-                  // ** Insert content here!
-                  child: Stack(
-                    alignment: Alignment.center,
-                    children: [
-                      Positioned(
-                        top: 0,
-                        right: -15,
-                        child: Container(
-                          height: 210,
-                          width: 135,
-                          decoration: const BoxDecoration(
-                            image: DecorationImage(
-                                image: AssetImage(
-                                    'assets/image_option/Ellipse 24.png'),
-                                fit: BoxFit.cover),
-                          ),
-                        ),
-                      ),
-                      Positioned(
-                        top: 160,
-                        left: 80,
-                        child: Container(
-                          height: 190,
-                          width: 165,
-                          decoration: const BoxDecoration(
-                            image: DecorationImage(
-                                image: AssetImage(
-                                    'assets/image_option/Ellipse 26.png'),
-                                fit: BoxFit.cover),
-                          ),
-                        ),
-                      ),
-                      Positioned(
-                        top: 0,
-                        left: -6,
-                        child: Container(
-                          height: 160,
-                          width: 105,
-                          decoration: const BoxDecoration(
-                            image: DecorationImage(
-                                image: AssetImage(
-                                    'assets/image_option/Ellipse 25.png'),
-                                fit: BoxFit.cover),
-                          ),
-                        ),
-                      ),
-                      const Positioned(
-                        top: 15,
-                        child: Center(
-                          child: Text(
-                            "Legal Ease",
-                            style: TextStyle(
-                              fontSize: 20,
-                              fontFamily: "Questrial",
-                              color: Color(0xD90C2924),
-                            ),
-                          ),
-                        ),
-                      ),
-                      const Positioned(
-                        top: 35,
-                        child: Center(
-                          child: Text(
-                            "Ius Criminales",
-                            style: TextStyle(
-                              fontSize: 12,
-                              fontFamily: "Questrial",
-                              color: Color(0xD90C2924),
-                            ),
-                          ),
-                        ),
-                      ),
-                      Positioned(
-                        top: 20,
-                        left: 14,
-                        child: GestureDetector(
+              GestureDetector(
+                onTap: () {
+                  if (!textFieldFocus.hasPrimaryFocus) {
+                    textFieldFocus.unfocus();
+                  }
+                },
+                child: Container(
+                    height: screenHeight,
+                    width: screenWidth,
+                    decoration: const BoxDecoration(
+                      image: DecorationImage(
+                          image: AssetImage('assets/image_option/OPTIONAL.png'),
+                          fit: BoxFit.cover),
+                    ),
+                    // ** Insert content here!
+                    child: Stack(
+                      alignment: Alignment.center,
+                      children: [
+                        Positioned(
+                          top: 0,
+                          right: -15,
                           child: Container(
-                            height: 25,
-                            width: 25,
-                            decoration: BoxDecoration(
-                              color: const Color(0xCCD9D9D9),
-                              borderRadius: BorderRadius.circular(5.0),
-                            ),
-                            child: const Icon(
-                              Icons.grid_view_rounded,
-                              size: 20,
-                              color: Color(0xCC0C2924),
+                            height: 210,
+                            width: 135,
+                            decoration: const BoxDecoration(
+                              image: DecorationImage(
+                                  image: AssetImage(
+                                      'assets/image_option/Ellipse 24.png'),
+                                  fit: BoxFit.cover),
                             ),
                           ),
-                          onTap: () {
-                            Navigator.push(context,
-                                MaterialPageRoute(builder: (context) {
-                              return const Sidenav();
-                            }));
-                          },
                         ),
-                      ),
-                      const Positioned(
-                        top: 98,
-                        right: 16,
-                        child: _TextField(),
-                      ),
-                      const GenerateBtn()
-                    ],
-                  )),
+                        Positioned(
+                          top: 160,
+                          left: 80,
+                          child: Container(
+                            height: 190,
+                            width: 165,
+                            decoration: const BoxDecoration(
+                              image: DecorationImage(
+                                  image: AssetImage(
+                                      'assets/image_option/Ellipse 26.png'),
+                                  fit: BoxFit.cover),
+                            ),
+                          ),
+                        ),
+                        Positioned(
+                          top: 0,
+                          left: -6,
+                          child: Container(
+                            height: 160,
+                            width: 105,
+                            decoration: const BoxDecoration(
+                              image: DecorationImage(
+                                  image: AssetImage(
+                                      'assets/image_option/Ellipse 25.png'),
+                                  fit: BoxFit.cover),
+                            ),
+                          ),
+                        ),
+                        const Positioned(
+                          top: 15,
+                          child: Center(
+                            child: Text(
+                              "Legal Ease",
+                              style: TextStyle(
+                                fontSize: 20,
+                                fontFamily: "Questrial",
+                                color: Color(0xD90C2924),
+                              ),
+                            ),
+                          ),
+                        ),
+                        const Positioned(
+                          top: 35,
+                          child: Center(
+                            child: Text(
+                              "Ius Criminales",
+                              style: TextStyle(
+                                fontSize: 12,
+                                fontFamily: "Questrial",
+                                color: Color(0xD90C2924),
+                              ),
+                            ),
+                          ),
+                        ),
+                        Positioned(
+                          top: 20,
+                          left: 14,
+                          child: GestureDetector(
+                            child: Container(
+                              height: 25,
+                              width: 25,
+                              decoration: BoxDecoration(
+                                color: const Color(0xCCD9D9D9),
+                                borderRadius: BorderRadius.circular(5.0),
+                              ),
+                              child: const Icon(
+                                Icons.grid_view_rounded,
+                                size: 20,
+                                color: Color(0xCC0C2924),
+                              ),
+                            ),
+                            onTap: () {
+                              Navigator.push(context,
+                                  MaterialPageRoute(builder: (context) {
+                                return const Sidenav();
+                              }));
+                            },
+                          ),
+                        ),
+                        const Positioned(
+                          top: 98,
+                          right: 16,
+                          child: _TextField(),
+                        ),
+                        const GenerateBtn()
+                      ],
+                    )),
+              ),
               //----------------------------------------------------------------------------------------------------
               Positioned(
                 top: 210,
-                child: Container(
-                  height: screenHeight - 300,
-                  width: screenWidth,
-                  decoration: BoxDecoration(
-                    color: const Color(0xE6FFFFFF),
-                    borderRadius: BorderRadius.circular(10.0),
-                  ),
-                  // ** Insert content here!
-                  child: Stack(
-                    alignment: Alignment.center,
-                    children: const [
-                      Positioned(
-                        top: 10,
-                        left: 13,
-                        child: Text(
-                          "Categories",
-                          style: TextStyle(
-                              fontSize: 18,
-                              color: Color(0xff0C2924),
-                              fontFamily: "RobotoFlex"),
+                child: GestureDetector(
+                  onTap: () {
+                    if (!textFieldFocus.hasPrimaryFocus) {
+                      textFieldFocus.unfocus();
+                    }
+                  },
+                  child: Container(
+                    height: screenHeight - 300,
+                    width: screenWidth,
+                    decoration: BoxDecoration(
+                      color: const Color(0xE6FFFFFF),
+                      borderRadius: BorderRadius.circular(10.0),
+                    ),
+                    // ** Insert content here!
+                    child: Stack(
+                      alignment: Alignment.center,
+                      children: const [
+                        Positioned(
+                          top: 10,
+                          left: 13,
+                          child: Text(
+                            "Categories",
+                            style: TextStyle(
+                                fontSize: 18,
+                                color: Color(0xff0C2924),
+                                fontFamily: "RobotoFlex"),
+                          ),
                         ),
-                      ),
-                      FirstRowBtn(),
-                      SecondRowBtn(),
-                      FirstRowIcon(),
-                      SecondRowIcon()
-                      
-                    ],
+                        FirstRowBtn(),
+                        SecondRowBtn(),
+                        FirstRowIcon(),
+                        SecondRowIcon()
+                      ],
+                    ),
                   ),
                 ),
               ),
               //----------------------------------------------------------------------------------------------------
               Align(
                 alignment: Alignment.bottomCenter,
-                child: Container(
-                  width: screenWidth,
-                  height: screenHeight - 480,
-                  decoration: const BoxDecoration(
-                    color: Color(0xFFFFFFFF),
-                    borderRadius: BorderRadius.only(
-                      topLeft: Radius.circular(10),
-                      topRight: Radius.circular(10),
+                child: GestureDetector(
+                  onTap: () {
+                    if (!textFieldFocus.hasPrimaryFocus) {
+                      textFieldFocus.unfocus();
+                    }
+                  },
+                  child: Container(
+                    width: screenWidth,
+                    height: screenHeight - 480,
+                    decoration: const BoxDecoration(
+                      color: Color(0xFFFFFFFF),
+                      borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(10),
+                        topRight: Radius.circular(10),
+                      ),
                     ),
+                    // ** Insert content here!
+                    child: null,
                   ),
-                  // ** Insert content here!
-                  child: null,
                 ),
               )
             ],
@@ -228,9 +251,9 @@ class __TextFieldState extends State<_TextField> {
         ],
       ),
       width: MediaQuery.of(context).size.width - 30,
-      child: SingleChildScrollView(
-        child: Center(
-            child: TextField(
+      child: Center(
+        child: TextField(
+          controller: _textEditingController,
           focusNode: _focusNode,
           minLines: 1,
           maxLines: 1,
@@ -243,7 +266,7 @@ class __TextFieldState extends State<_TextField> {
             hintText: "Enter concerns",
             fillColor: const Color(0xffD9D9D9),
           ),
-        )),
+        ),
       ),
     );
   }
@@ -261,14 +284,20 @@ class GenerateBtn extends StatelessWidget {
         height: 35,
         child: ElevatedButton(
           style: ButtonStyle(
-              alignment: Alignment.center,
-              backgroundColor:
-                  MaterialStateProperty.all(const Color(0xffD9D9D9)),
-              shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                  RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(15.0),
-                      side: const BorderSide(color: Color(0xff97a294))))),
-          onPressed: () {},
+            alignment: Alignment.center,
+            backgroundColor: MaterialStateProperty.all(const Color(0xffD9D9D9)),
+            shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+              RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(15.0),
+                side: const BorderSide(
+                  color: Color(0xff97a294),
+                ),
+              ),
+            ),
+          ),
+          onPressed: () {
+            _textEditingController.clear();
+          },
           child: const Text(
             "Generate",
             style: TextStyle(color: Color(0xff000000)),
