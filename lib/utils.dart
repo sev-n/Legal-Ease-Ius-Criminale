535bd1dabdea242a1c0379c60248cea12798104e
sev-n <nielsevilla39@gmail.com>
2023-04-08 13:20:30 +0800

    NOT FINAL: Try to separate the design format

---
+++ lib/utils.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class FormatUtil {
+  // general
+  static const Color color = Color(0xCC0C2924);
+  static final TextStyle font = GoogleFonts.robotoFlex();
+  static double normalSize = 12.sp;
+  static double headSize = 20.sp;
+
+  // home page
+  static int contSizeCategories = 75;
+  static int sizeBoxCategories = 30;
+}
