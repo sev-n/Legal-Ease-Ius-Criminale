6e77e583501fbbe7e6b701a5d249bf82974fe797
sev-n <nielsevilla39@gmail.com>
2023-05-28 20:52:15 +0800

    implement loading animation

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:get/get.dart';
+import 'package:legalease_matrimonial/src/model/loading_state.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'src/home/welcome_screen.dart';
+import 'package:provider/provider.dart';
 
 Future main() async {
   WidgetsFlutterBinding
@@ -16,29 +18,35 @@ Future main() async {
 
   final prefs = await SharedPreferences.getInstance();
 
-  runApp(MyApp(prefs: prefs));
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  final SharedPreferences prefs;
+  //final SharedPreferences prefs;
 
-  const MyApp({super.key, required this.prefs});
+  const MyApp({super.key});
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return ScreenUtilInit(
-      designSize: const Size(390, 844),
-      builder: (context, child) {
-        return GetMaterialApp(
-          debugShowCheckedModeBanner: false,
-          title: "Legal ease",
-          theme: ThemeData(
-            primarySwatch: Colors.blue,
+    return MultiProvider(
+        providers: [
+          ChangeNotifierProvider<LoadingState>(
+            create: (_) => LoadingState(),
           ),
-          home: OneTimeWelcomePage(prefs: prefs),
-        );
-      },
-    );
+        ],
+        child: ScreenUtilInit(
+          designSize: const Size(390, 844),
+          builder: (context, child) {
+            return GetMaterialApp(
+              debugShowCheckedModeBanner: false,
+              title: "Legal ease",
+              theme: ThemeData(
+                primarySwatch: Colors.blue,
+              ),
+              home: const WelcomePage(),
+            );
+          },
+        ));
   }
 }
--- lib/src/home/welcome_screen.dart
+++ lib/src/home/welcome_screen.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:smooth_page_indicator/smooth_page_indicator.dart';
+import '../model/loading_state.dart';
 import '../pages/welcome_pages/welcome_page_1.dart';
 import '../pages/welcome_pages/welcome_page_2.dart';
 import '../pages/welcome_pages/welcome_page_3.dart';
 import 'home.dart';
+import 'package:lottie/lottie.dart';
+import 'package:provider/provider.dart';
 
 class OneTimeWelcomePage extends StatelessWidget {
   final SharedPreferences prefs;
@@ -66,6 +70,18 @@ class _WelcomePageState extends State<WelcomePage> {
               ),
             ),
             isLastPage ? const GetStartedBtn() : const NextBtn(),
+            isLastPage
+                ? SizedBox(
+                    child: Consumer<LoadingState>(
+                      builder: (context, data, child) {
+                        return Visibility(
+                          visible: !data.isShowed,
+                          child: const Loading(),
+                        );
+                      },
+                    ),
+                  )
+                : const SizedBox(),
             Container(
               alignment: const Alignment(0, 0.65),
               child: const Row(
@@ -134,34 +150,54 @@ class GetStartedBtn extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Align(
-      alignment: const Alignment(0, 0.85),
-      child: SizedBox(
-        width: 200,
-        child: ElevatedButton(
-          onPressed: () {
-            Navigator.of(context).pushReplacement(
-              MaterialPageRoute(builder: (context) => const HomePage()),
-            );
-          },
-          style: ElevatedButton.styleFrom(
-            backgroundColor: const Color(0xffE8E8E8),
-            foregroundColor: Colors.black,
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(30.0),
+    var varShow = Provider.of<LoadingState>(context, listen: false);
+
+    void sleep() {
+      Future.delayed(const Duration(seconds: 10), () {
+        // code here you want to do after sleep
+        debugPrint('This code execute');
+        debugPrint(varShow.show.toString());
+        Navigator.of(context).pushReplacement(
+          MaterialPageRoute(builder: (context) => const HomePage()),
+        );
+      });
+    }
+
+    return Consumer<LoadingState>(
+      builder: (context, data, child) {
+        return Visibility(
+          visible: data.isShowed,
+          child: Align(
+            alignment: const Alignment(0, 0.85),
+            child: SizedBox(
+              width: 200,
+              child: ElevatedButton(
+                onPressed: () {
+                  sleep();
+                  debugPrint('This also code execute');
+                  varShow.setShow(false);
+                },
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: const Color(0xffE8E8E8),
+                  foregroundColor: Colors.black,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(30.0),
+                  ),
+                ),
+                child: const Text(
+                  "Get Started",
+                  style: TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.w500,
+                    fontFamily: "RobotoFlex",
+                    color: Color(0xff0C2924),
+                  ),
+                ),
+              ),
             ),
           ),
-          child: const Text(
-            "Get Started",
-            style: TextStyle(
-              fontSize: 18,
-              fontWeight: FontWeight.w500,
-              fontFamily: "RobotoFlex",
-              color: Color(0xff0C2924),
-            ),
-          ),
-        ),
-      ),
+        );
+      },
     );
   }
 }
@@ -188,6 +224,27 @@ class _PageIndicatorState extends State<PageIndicator> {
         strokeWidth: 1.5,
         dotColor: Color(0xff0C2924),
         activeDotColor: Color(0xff0C2924),
+      ),
+    );
+  }
+}
+
+class Loading extends StatefulWidget {
+  const Loading({super.key});
+
+  @override
+  State<Loading> createState() => _LoadingState();
+}
+
+class _LoadingState extends State<Loading> {
+  @override
+  Widget build(BuildContext context) {
+    return Align(
+      alignment: const Alignment(0, 0.88),
+      child: Lottie.asset(
+        'assets/lottie/loading13.json',
+        width: 100.w,
+        height: 100.h,
       ),
     );
   }
