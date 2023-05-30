ddd74de1035455fc8500cd8eb303f7da46530716
sev-n <nielsevilla39@gmail.com>
2023-05-30 23:18:22 +0800

    added 11-15 sections

--- lib/src/pages/contents/categories_contents/categories_sections/inchoate_crime/republic_9262/section_11-15.dart
+++ lib/src/pages/contents/categories_contents/categories_sections/inchoate_crime/republic_9262/section_11-15.dart
@@ -64,7 +64,8 @@ class SectionEleven extends StatelessWidget {
                           child: Column(
                             children: [
                               Text(
-                                'Section 1. Short Title.',
+                                'Section 11. How to Apply for a Protection Order.',
+                                textAlign: TextAlign.center,
                                 style: TextStyle(
                                   fontFamily: "RobotoFlex",
                                   fontSize: 20.sp,
@@ -74,7 +75,29 @@ class SectionEleven extends StatelessWidget {
                               ),
                               SizedBox(height: 20.h),
                               Text(
-                                'This Act shall be known as the "Anti-Violence Against Women and Their Children Act of 2004."',
+                                'The application for a protection order must be in '
+                                'writing, signed and verified under oath by the applicant. It may be filed as an independent action '
+                                'or as incidental relief in any civil or criminal case the subject matter or issues thereof partakes of '
+                                'a violence as described in this Act. A standard protection order application form, written in '
+                                'English with translation to the major local languages, shall be made available to facilitate '
+                                'applications for protections order, and shall contain, among other, the following information\n\n'
+                                '(a) names and addresses of petitioner and respondent;\n\n'
+                                '(b) description of relationships between petitioner and respondent;\n\n'
+                                '(c) a statement of the circumstances of the abuse;\n\n'
+                                '(d) description of the reliefs requested by petitioner as specified in Sec. 8 herein;\n\n'
+                                '(e) request for counsel and reasons for such;\n\n'
+                                '(f) request for waiver of application fees until hearing; and\n\n'
+                                '(g) an attestation that there is no pending application for a protection order in another court. '
+                                'If the applicants is not the victim, the application must be accompanied by an affidavit of the '
+                                'applicant attesting to (a) the circumstances of the abuse suffered by the victim and (b) the '
+                                'circumstances of consent given by the victim for the filling of the application. When disclosure of '
+                                'the address of the victim will pose danger to her life, it shall be so stated in the application. In '
+                                'such a case, the applicant shall attest that the victim is residing in the municipality or city over '
+                                'which court has territorial jurisdiction, and shall provide a mailing address for purpose of service '
+                                'processing. An application for protection order filed with a court shall be considered an application for both a TPO and PPO. \n'
+                                'Barangay officials and court personnel shall assist applicants in the preparation of the '
+                                'application. Law enforcement agents shall also extend assistance in the application for '
+                                'protection orders in cases brought to their attention.\n',
                                 textAlign: TextAlign.justify,
                                 style: TextStyle(
                                   fontFamily: "RobotoFlex",
@@ -160,7 +183,8 @@ class SectionTwelve extends StatelessWidget {
                           child: Column(
                             children: [
                               Text(
-                                'Section 1. Short Title.',
+                                'Section 12. Enforceability of Protection Orders.',
+                                textAlign: TextAlign.center,
                                 style: TextStyle(
                                   fontFamily: "RobotoFlex",
                                   fontSize: 20.sp,
@@ -170,7 +194,10 @@ class SectionTwelve extends StatelessWidget {
                               ),
                               SizedBox(height: 20.h),
                               Text(
-                                'This Act shall be known as the "Anti-Violence Against Women and Their Children Act of 2004."',
+                                'All TPOs and PPOs issued under this Act shall '
+                                'be enforceable anywhere in the Philippines and a violation thereof shall be punishable with a '
+                                'fine ranging from Five Thousand Pesos (P5,000.00) to Fifty Thousand Pesos (P50,000.00) '
+                                'and/or imprisonment of six (6) months.\n',
                                 textAlign: TextAlign.justify,
                                 style: TextStyle(
                                   fontFamily: "RobotoFlex",
@@ -256,7 +283,8 @@ class SectionThirteen extends StatelessWidget {
                           child: Column(
                             children: [
                               Text(
-                                'Section 1. Short Title.',
+                                'Section 13.  Legal Representation of Petitioners for Protection Order.',
+                                textAlign: TextAlign.center,
                                 style: TextStyle(
                                   fontFamily: "RobotoFlex",
                                   fontSize: 20.sp,
@@ -266,7 +294,15 @@ class SectionThirteen extends StatelessWidget {
                               ),
                               SizedBox(height: 20.h),
                               Text(
-                                'This Act shall be known as the "Anti-Violence Against Women and Their Children Act of 2004."',
+                                'If the woman or her child '
+                                'requests in the applications for a protection order for the appointment of counsel because of '
+                                'lack of economic means to hire a counsel de parte, the court shall immediately direct the Public '
+                                "Attorney's Office (PAO) to represent the petitioner in the hearing on the application. If the PAO "
+                                'determines that the applicant can afford to hire the services of a counsel de parte, it shall '
+                                'facilitate the legal representation of the petitioner by a counsel de parte. The lack of access to '
+                                'family or conjugal resources by the applicant, such as when the same are controlled by the '
+                                'perpetrator, shall qualify the petitioner to legal representation by the PAO.\n'
+                                'However, a private counsel offering free legal service is not barred from representing the petitioner.\n',
                                 textAlign: TextAlign.justify,
                                 style: TextStyle(
                                   fontFamily: "RobotoFlex",
@@ -352,7 +388,8 @@ class SectionFourteen extends StatelessWidget {
                           child: Column(
                             children: [
                               Text(
-                                'Section 1. Short Title.',
+                                'Section 14. Barangay Protection Orders (BPOs); Who May Issue and How.',
+                                textAlign: TextAlign.center,
                                 style: TextStyle(
                                   fontFamily: "RobotoFlex",
                                   fontSize: 20.sp,
@@ -362,7 +399,25 @@ class SectionFourteen extends StatelessWidget {
                               ),
                               SizedBox(height: 20.h),
                               Text(
-                                'This Act shall be known as the "Anti-Violence Against Women and Their Children Act of 2004."',
+                                'Barangay Protection '
+                                'Orders (BPOs) refer to the protection order issued by the Punong Barangay ordering the '
+                                'perpetrator to desist from committing acts under Sec. 5 (a) and (b) of this Act. A Punong '
+                                'Barangay who receives applications for a BPO shall issue the protection order to the applicant '
+                                'on the date of filing after ex parte determination of the basis of the application. If the Punong '
+                                'Barangay is unavailable to act on the application for a BPO, the application shall be acted upon '
+                                'by any available Barangay Kagawad. If the BPO is issued by a Barangay Kagawad the order '
+                                'must be accompanied by an attestation by the Barangay Kagawad that the Punong Barangay '
+                                'was unavailable at the time for the issuance of the BPO. BPOs shall be effective for fifteen (15) '
+                                'days. Immediately after the issuance of an ex parte BPO, the Punong Barangay or Barangay '
+                                'Kagawad shall personally serve a copy of the same on the respondent, or direct any barangay '
+                                'official to effect is personal service.\n'
+                                'The parties may be accompanied by a non-lawyer advocate in any proceeding before the Punong Barangay.\n'
+
+
+                                
+                                
+                                
+                                ,
                                 textAlign: TextAlign.justify,
                                 style: TextStyle(
                                   fontFamily: "RobotoFlex",
@@ -448,7 +503,7 @@ class SectionFifteen extends StatelessWidget {
                           child: Column(
                             children: [
                               Text(
-                                'Section 1. Short Title.',
+                                'Section 15. Temporary Protection Orders.',
                                 style: TextStyle(
                                   fontFamily: "RobotoFlex",
                                   fontSize: 20.sp,
@@ -458,7 +513,16 @@ class SectionFifteen extends StatelessWidget {
                               ),
                               SizedBox(height: 20.h),
                               Text(
-                                'This Act shall be known as the "Anti-Violence Against Women and Their Children Act of 2004."',
+                                'Temporary Protection Orders (TPOs) refers to the '
+                                'protection order issued by the court on the date of filing of the application after ex parte '
+                                'determination that such order should be issued. A court may grant in a TPO any, some or all of '
+                                'the reliefs mentioned in this Act and shall be effective for thirty (30) days. The court shall '
+                                'schedule a hearing on the issuance of a PPO prior to or on the date of the expiration of the '
+                                'TPO. The court shall order the immediate personal service of the TPO on the respondent by the '
+                                'court sheriff who may obtain the assistance of law enforcement agents for the service. The TPO '
+                                'shall include notice of the date of the hearing on the merits of the issuance of a PPO.\n'
+                                
+                                ,
                                 textAlign: TextAlign.justify,
                                 style: TextStyle(
                                   fontFamily: "RobotoFlex",
