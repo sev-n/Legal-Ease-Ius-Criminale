90199e7a2de84f3040e599f7240ecea9d6f10d92
sev-n <nielsevilla39@gmail.com>
2023-06-01 18:06:40 +0800

    update model list

--- lib/src/model/against_property_sections.dart
+++ lib/src/model/against_property_sections.dart
@@ -1,6 +1,102 @@
+import 'package:legalease_matrimonial/src/pages/contents/categories_contents/categories_sections/against_property/republic_166/chapter_I/section_1.dart';
+import 'package:legalease_matrimonial/src/pages/contents/categories_contents/categories_sections/against_property/republic_166/chapter_II/section_2.dart';
+import 'package:legalease_matrimonial/src/pages/contents/categories_contents/categories_sections/against_property/republic_166/chapter_III/section_3.dart';
+import 'package:legalease_matrimonial/src/pages/contents/categories_contents/categories_sections/against_property/republic_166/chapter_IV/section_4.dart';
+import 'package:legalease_matrimonial/src/pages/contents/categories_contents/categories_sections/against_property/republic_166/chapter_IX/section_9.dart';
+import 'package:legalease_matrimonial/src/pages/contents/categories_contents/categories_sections/against_property/republic_166/chapter_V/section_5.dart';
+import 'package:legalease_matrimonial/src/pages/contents/categories_contents/categories_sections/against_property/republic_166/chapter_VI/section_6.dart';
+import 'package:legalease_matrimonial/src/pages/contents/categories_contents/categories_sections/against_property/republic_166/chapter_VII/section_7.dart';
+import 'package:legalease_matrimonial/src/pages/contents/categories_contents/categories_sections/against_property/republic_166/chapter_VIII/chapter_8.dart';
+import 'package:legalease_matrimonial/src/pages/contents/categories_contents/categories_sections/against_property/republic_166/chapter_X/section_10.dart';
+import 'package:legalease_matrimonial/src/pages/contents/categories_contents/categories_sections/against_property/republic_166/chapter_XI/section_11.dart';
+import 'package:legalease_matrimonial/src/pages/contents/categories_contents/categories_sections/against_property/republic_166/chapter_XII/section_12.dart';
+import 'package:legalease_matrimonial/src/pages/contents/categories_contents/categories_sections/against_property/republic_166/chapter_XIII/section_13.dart';
+import 'package:legalease_matrimonial/src/pages/contents/categories_contents/categories_sections/against_property/republic_166/chapter_XIV/section_14.dart';
+
 import 'inchoate_sections.dart';
 
 // REPUBLIC ACT NO. 166
 class ListOfChaptersProperty {
-  static List<Sections> section = [];
+  static List<Sections> chapters = [
+    Sections(title: 'CHAPTER I', description: 'Powers, Duties and Functions of Patent Office'),
+    Sections(title: 'CHAPTER II', description: 'Registration of Marks and Trade-Names'),
+    Sections(title: 'CHAPTER III', description: 'Renewals of Certificate of Registration'),
+    Sections(title: 'CHAPTER IV', description: 'Cancellation of Registration'),
+    Sections(title: 'CHAPTER V', description: 'Rights and Remedies'),
+    Sections(title: 'CHAPTER VI', description: 'Unfair Competition'),
+    Sections(title: 'CHAPTER VII', description: 'False Designation of Origin and False Description'),
+    Sections(title: 'CHAPTER VIII', description: 'Assignment and Transmission of Rights'),
+    Sections(title: 'CHAPTER IX', description: 'Review of Orders or Decisions of Director'),
+    Sections(title: 'CHAPTER X', description: 'Importations Prohibited'),
+    Sections(title: 'CHAPTER XI', description: 'Provisions in Reference to Foreign Industrial Property'),
+    Sections(title: 'CHAPTER XII', description: 'Construction and Definitions'),
+    Sections(title: 'CHAPTER XIII', description: 'Fees'),
+    Sections(title: 'CHAPTER XIV', description: 'Miscellaneous Provisions'),
+  ];
+
+  static List<Sections> sections = [
+    Sections(title: 'Section 1', description: 'Transfer of powers from Bureau of Commerce to Patent Office.'),
+    Sections(title: 'Section 2', description: 'What are registrable'),
+    Sections(title: 'Section 3', description: 'Application by non-residents'),
+    Sections(title: 'Section 4', description: 'Registration of trade-marks, trade-names and service-marks'),
+    Sections(title: 'Section 5', description: 'Requirements of the application'),
+    Sections(title: 'Section 6', description: 'Classification of goods and services'),
+    Sections(title: 'Section 7', description: 'Examination and publication'),
+    Sections(title: 'Section 8', description: 'Opposition'),
+    Sections(title: 'Section 9', description: 'Notice and hearing'),
+    Sections(title: 'Section 10', description: 'Issuance and publication of certificates'),
+    Sections(title: 'Section 11', description: 'Issuance and contents of the certificate'),
+    Sections(title: 'Section 12', description: 'Duration'),
+    Sections(title: 'Section 13', description: 'Disclaimers before issue'),
+    Sections(title: 'Section 14', description: 'Voluntary surrender, cancellation, amendment and disclaimer after registration'),
+    Sections(title: 'Section 15', description: 'Renewal'),
+    Sections(title: 'Section 16', description: 'Effect of failure to renew registration'),
+    Sections(title: 'Section 17', description: 'Grounds for cancellation'),
+    Sections(title: 'Section 18', description: 'Requirements of petition; notice and heating'),
+    Sections(title: 'Section 19', description: 'Cancellation of registration'),
+    Sections(title: 'Section 20', description: 'Certificate of registration prima facie evidence of validity'),
+    Sections(title: 'Section 21', description: 'Requirements of notice of registration of trademark'),
+    Sections(title: 'Section 22', description: 'Infringement, what constitutes'),
+    Sections(title: 'Section 23', description: 'Actions, and damages and injunction for infringement'),
+    Sections(title: 'Section 24', description: 'Power of court to order infringing material destroyed'),
+    Sections(title: 'Section 25', description: 'Authority to determine right to registration'),
+    Sections(title: 'Section 26', description: 'Action for false or fraudulent declaration'),
+    Sections(title: 'Section 27', description: 'Jurisdiction of Court of First Instance'),
+    Sections(title: 'Section 28', description: 'Appeal'),
+    Sections(title: 'Section 29', description: 'Unfair competition, rights and remedies'),
+    Sections(title: 'Section 30', description: 'False designation of origin and false description forbidden'),
+    Sections(title: 'Section 31', description: 'Rights assignable and form of assignment'),
+    Sections(title: 'Section 32', description: 'Issuance of certificate of registration to assignee'),
+    Sections(title: 'Section 33', description: 'Appeal from action of Director'),
+    Sections(title: 'Section 34', description: 'Procedure on appeal'),
+    Sections(title: 'Section 35', description: 'Goods bearing infringing marks or trade names'),
+    Sections(title: 'Section 36', description: 'Goods with false designation of origin and false description'),
+    Sections(title: 'Section 37', description: 'Rights of foreign registrants'),
+    Sections(title: 'Section 38', description: 'Words and terms defined and construed'),
+    Sections(title: 'Section 39', description: 'Fees'),
+    Sections(title: 'Section 40', description: 'Collective marks and collective trade-names'),
+    Sections(title: 'Section 41', description: 'Reservation in favor of prior registration'),
+    Sections(title: 'Section 42', description: 'Renewal of registrations which expired during the war'),
+    Sections(title: 'Section 43', description: 'Pending applications'),
+    Sections(title: 'Section 44', description: 'Repealing clause'),
+    Sections(title: 'Section 45', description: 'Effective date'),
+  ];
+
+  static List routesPerChapter = const [
+    Section1(),
+    Section2(),
+    Section3(),
+    Section4(),
+    Section5(),
+    Section6(),
+    Section7(),
+    Section8(),
+    Section9(),
+    Section10(),
+    Section11(),
+    Section12(),
+    Section13(),
+    Section14(),
+  ];
 }
+
