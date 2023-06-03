9adcae592bbe8b8428b988b52d8a807d55331706
sev-n <nielsevilla39@gmail.com>
2023-06-03 08:54:33 +0800

    Added expansion panel

--- lib/src/pages/contents/categories_contents/against_person.dart
+++ lib/src/pages/contents/categories_contents/against_person.dart
@@ -2,8 +2,21 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:legalease_matrimonial/src/pages/contents/categories_contents/categories_sections/againts_person/republic_9745/sections.dart';
 
-class PersonContent extends StatelessWidget {
+class PersonContent extends StatefulWidget {
   const PersonContent({super.key});
+
+  @override
+  State<PersonContent> createState() => _PersonContentState();
+}
+
+class _PersonContentState extends State<PersonContent> {
+  bool isExpanded9745 = false;
+
+  void toggleExpansion9745() {
+    setState(() {
+      isExpanded9745 = !isExpanded9745;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -59,36 +72,60 @@ class PersonContent extends StatelessWidget {
                         ),
                       ),
                       child: ListView(
-                        padding: EdgeInsets.only(left: 5.w, right: 5.w),
+                        padding:
+                            EdgeInsets.only(top: 10.h, left: 5.w, right: 5.w),
                         children: [
-                          Divider(
-                            thickness: 1.0,
-                            indent: 16.0,
-                            endIndent: 16.0,
-                            color: Colors.grey[500],
-                          ),
-                          ListTile(
-                            title: const Text(
-                              "REPUBLIC ACT NO. 9745",
-                              style: TextStyle(
-                                fontWeight: FontWeight.bold,
-                              ),
-                            ),
-                            onTap: () {
-                              Navigator.push(
-                                context,
-                                MaterialPageRoute(
-                                  builder: (context) =>
-                                      const SectionsPerson(),
-                                ),
-                              );
+                          ExpansionPanelList(
+                            elevation: 0,
+                            dividerColor: Colors.transparent,
+                            expandedHeaderPadding: EdgeInsets.zero,
+                            animationDuration:
+                                const Duration(milliseconds: 400),
+                            expansionCallback:
+                                (int panelIndex, bool isExpanded) {
+                              toggleExpansion9745();
                             },
-                          ),
-                          Divider(
-                            thickness: 1.0,
-                            indent: 16.0,
-                            endIndent: 16.0,
-                            color: Colors.grey[500],
+                            children: [
+                              ExpansionPanel(
+                                backgroundColor: const Color(0xffE8E8E8),
+                                canTapOnHeader: false,
+                                headerBuilder: (context, isExpanded) {
+                                  return ListTile(
+                                    title: const Text(
+                                      "REPUBLIC ACT NO. 9745",
+                                      style: TextStyle(
+                                        fontFamily: "RobotoFlex",
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                    ),
+                                    onTap: () {
+                                      Navigator.push(
+                                        context,
+                                        MaterialPageRoute(
+                                          builder: (context) =>
+                                              const SectionsPerson(),
+                                        ),
+                                      );
+                                    },
+                                  );
+                                },
+                                body: Padding(
+                                  padding: EdgeInsets.only(left: 5.w, right: 5.w),
+                                  child: const Text(
+                                    "\nAN ACT PENALIZING TORTURE AND OTHER CRUEL "
+                                    "INHUMAN AND DEGRADING TREATMENT OR "
+                                    "PUNISHMENT AND PRESCRIBING PENALTIES THEREFORE\n\n",
+                                    textAlign: TextAlign.center,
+                                    style: TextStyle(
+                                        fontFamily: "RobotoFlex",
+                                        fontWeight: FontWeight.w500,
+                                        height: 1.4,
+                                        ),
+                                  ),
+                                ),
+                                isExpanded: isExpanded9745,
+                              ),
+                            ],
                           ),
                         ],
                       ),
@@ -103,5 +140,3 @@ class PersonContent extends StatelessWidget {
     );
   }
 }
-
-
