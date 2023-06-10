b858a110bf131514ede4f85f115b0d1c8b1e9ebe
sev-n <nielsevilla39@gmail.com>
2023-06-10 14:54:05 +0800

    update model for ra 7610

--- lib/src/model/against_person_sections.dart
+++ lib/src/model/against_person_sections.dart
@@ -1,3 +1,18 @@
+import 'package:legalease_matrimonial/src/pages/contents/categories_contents/categories_sections/againts_person/republic_7610/chapter_I/section_1.dart';
+import 'package:legalease_matrimonial/src/pages/contents/categories_contents/categories_sections/againts_person/republic_7610/chapter_II/section_2.dart';
+import 'package:legalease_matrimonial/src/pages/contents/categories_contents/categories_sections/againts_person/republic_7610/chapter_III/section_3.dart';
+import 'package:legalease_matrimonial/src/pages/contents/categories_contents/categories_sections/againts_person/republic_7610/chapter_IV/section_4.dart';
+import 'package:legalease_matrimonial/src/pages/contents/categories_contents/categories_sections/againts_person/republic_7610/chapter_IX/section_9.dart';
+import 'package:legalease_matrimonial/src/pages/contents/categories_contents/categories_sections/againts_person/republic_7610/chapter_V/section_5.dart';
+import 'package:legalease_matrimonial/src/pages/contents/categories_contents/categories_sections/againts_person/republic_7610/chapter_VI/section_6.dart';
+import 'package:legalease_matrimonial/src/pages/contents/categories_contents/categories_sections/againts_person/republic_7610/chapter_VII/section_7.dart';
+import 'package:legalease_matrimonial/src/pages/contents/categories_contents/categories_sections/againts_person/republic_7610/chapter_VIII/section_8.dart';
+import 'package:legalease_matrimonial/src/pages/contents/categories_contents/categories_sections/againts_person/republic_7610/chapter_X/section_10.dart';
+import 'package:legalease_matrimonial/src/pages/contents/categories_contents/categories_sections/againts_person/republic_7610/chapter_XI/section_11.dart';
+import 'package:legalease_matrimonial/src/pages/contents/categories_contents/categories_sections/againts_person/republic_7610/chapter_XII/section_12.dart';
+import 'package:legalease_matrimonial/src/pages/contents/categories_contents/categories_sections/againts_person/republic_7610/chapter_XIII/section_13.dart';
+import 'package:legalease_matrimonial/src/pages/contents/categories_contents/categories_sections/againts_person/republic_7610/contents/section_1-20.dart';
+import 'package:legalease_matrimonial/src/pages/contents/categories_contents/categories_sections/againts_person/republic_7610/contents/section_21-36.dart';
 import 'package:legalease_matrimonial/src/pages/contents/categories_contents/categories_sections/againts_person/republic_9745/section_1-10.dart';
 import 'package:legalease_matrimonial/src/pages/contents/categories_contents/categories_sections/againts_person/republic_9745/section_11-20.dart';
 import 'package:legalease_matrimonial/src/pages/contents/categories_contents/categories_sections/againts_person/republic_9745/section_21-27.dart';
@@ -64,5 +79,119 @@ class ListOfSectionsPerson {
     PersonSectionTwentyFive(),
     PersonSectionTwentySix(),
     PersonSectionTwentySeven(),
+  ];
+}
+
+
+//REPUBLIC ACT NO. 7610 
+class ListOfChaptersPerson {
+  static List<Sections> chapters = [
+    Sections(title: 'ARTICLE I', description: 'Title, Policy, Principles and Definitions of Terms'),
+    Sections(title: 'ARTICLE II', description: 'Program on Child Abuse, Exploitation and Discrimination'),
+    Sections(title: 'ARTICLE III', description: 'Child Prostitution and Other Sexual Abuse'),
+    Sections(title: 'ARTICLE IV', description: 'Child Trafficking'),
+    Sections(title: 'ARTICLE V', description: 'Obscene Publications and Indecent Shows'),
+    Sections(title: 'ARTICLE VI', description: 'Other Acts of Abuse'),
+    Sections(title: 'ARTICLE VII', description: 'Sanctions for Establishments or Enterprises'),
+    Sections(title: 'ARTICLE VIII', description: 'Working Children'),
+    Sections(title: 'ARTICLE IX', description: 'Children of Indigenous Cultural Communities'),
+    Sections(title: 'ARTICLE X', description: 'Children in Situations of Armed Conflict'),
+    Sections(title: 'ARTICLE XI', description: 'Remedial Procedures'),
+    Sections(title: 'ARTICLE XII', description: 'Common Penal Provisions'),
+    Sections(title: 'ARTICLE XIII', description: 'Final Provisions'),
+  ];
+
+  static List<Sections> sections = [
+    Sections(title: 'Section 1', description: 'Title'),
+    Sections(title: 'Section 2', description: 'Declaration of State Policy and Principles'),
+    Sections(title: 'Section 3', description: 'Definition of Terms'),
+    Sections(title: 'Section 4', description: 'Formulation of the Program'),
+    Sections(title: 'Section 5', description: 'Child Prostitution and Other Sexual Abuse'),
+    Sections(title: 'Section 6', description: 'Attempt To Commit Child Prostitution'),
+    Sections(title: 'Section 7', description: 'Child Trafficking'),
+    Sections(title: 'Section 8', description: 'Attempt to Commit Child Trafficking'),
+    Sections(title: 'Section 9', description: 'Obscene Publications and Indecent Shows'),
+    Sections(title: 'Section 10', description: "Other Acts of Neglect, Abuse, Cruelty or Exploitation and Other Conditions Prejudicial to the Child's Development"),
+    Sections(title: 'Section 11', description: 'Sanctions of Establishments or Enterprises which Promote, Facilitate, or Conduct Activities Constituting Child Prostitution and Other Sexual Abuse, Child Trafficking, Obscene Publications and Indecent Shows, and Other Acts of Abuse'),
+    Sections(title: 'Section 12', description: 'Employment of Children'),
+    Sections(title: 'Section 13', description: 'Non-formal Education for Working Children'),
+    Sections(title: 'Section 14', description: 'Prohibition on the Employment of Children in Certain Advertisements'),
+    Sections(title: 'Section 15', description: 'Duty of Employer'),
+    Sections(title: 'Section 16', description: 'Penalties'),
+    Sections(title: 'Section 17', description: 'Survival, Protection and Development'),
+    Sections(title: 'Section 18', description: 'System of and Access to Education'),
+    Sections(title: 'Section 19', description: 'Health and Nutrition'),
+    Sections(title: 'Section 20', description: 'Discrimination'),
+    Sections(title: 'Section 21', description: 'Participation'),
+    Sections(title: 'Section 22', description: 'Children as Zones of Peace'),
+    Sections(title: 'Section 23', description: 'Evacuation of Children During Armed Conflict'),
+    Sections(title: 'Section 24', description: 'Family Life and Temporary Shelter'),
+    Sections(title: 'Section 25', description: 'Rights of Children Arrested for Reasons Related to Armed Conflict'),
+    Sections(title: 'Section 26', description: 'Monitoring and Reporting of Children in Situations of Armed Conflict'),
+    Sections(title: 'Section 27', description: 'Who May File a Complaint'),
+    Sections(title: 'Section 28', description: 'Protective Custody of the Child'),
+    Sections(title: 'Section 29', description: 'Confidentiality'),
+    Sections(title: 'Section 30', description: 'Special Court Proceedings'),
+    Sections(title: 'Section 31', description: 'Common Penal Provisions'),
+    Sections(title: 'Section 32', description: 'Rules and Regulations'),
+    Sections(title: 'Section 33', description: 'Appropriations'),
+    Sections(title: 'Section 34', description: 'Separability Clause'),
+    Sections(title: 'Section 35', description: 'Repealing Clause'),
+    Sections(title: 'Section 36', description: 'Effectivity Clause'),
+  ];
+
+  static List routesPerChapterPerson = const [
+    SectionPerson1(),
+    SectionPerson2(),
+    SectionPerson3(),
+    SectionPerson4(),
+    SectionPerson5(),
+    SectionPerson6(),
+    SectionPerson7(),
+    SectionPerson8(),
+    SectionPerson9(),
+    SectionPerson10(),
+    SectionPerson11(),
+    SectionPerson12(),
+    SectionPerson13(),
+  ];
+
+  static List routesContentsPerSection = const [
+    PersonContentOne(),
+    PersonContentTwo(),
+    PersonContentThree(),
+    PersonContentFour(),
+    PersonContentFive(),
+    PersonContentSix(),
+    PersonContentSeven(),
+    PersonContentEight(),
+    PersonContentNine(),
+    PersonContentTen(),
+    PersonContentEleven(),
+    PersonContentTwelve(),
+    PersonContentThirteen(),
+    PersonContentFourteen(),
+    PersonContentFifteen(),
+    PersonContentSixteen(),
+    PersonContentSeventeen(),
+    PersonContentEighteen(),
+    PersonContentNineteen(),
+    PersonContentTwenty(),
+    PersonContentTwentyOne(),
+    PersonContentTwentyTwo(),
+    PersonContentTwentyThree(),
+    PersonContentTwentyFour(),
+    PersonContentTwentyFive(),
+    PersonContentTwentySix(),
+    PersonContentTwentySeven(),
+    PersonContentTwentyEight(),
+    PersonContentTwentyNine(),
+    PersonContentThirty(),
+    PersonContentThirtyOne(),
+    PersonContentThirtyTwo(),
+    PersonContentThirtyThree(),
+    PersonContentThirtyFour(),
+    PersonContentThirtyFive(),
+    PersonContentThirtySix(),
   ];
 }
