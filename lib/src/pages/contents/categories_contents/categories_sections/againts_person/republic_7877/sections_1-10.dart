bea89dd2d19d32d5e39bd66d0da6786b334e435c
sev-n <nielsevilla39@gmail.com>
2023-06-10 20:25:40 +0800

    ra no 7877 finished.

--- lib/src/pages/contents/categories_contents/categories_sections/againts_person/republic_7877/sections_1-10.dart
+++ lib/src/pages/contents/categories_contents/categories_sections/againts_person/republic_7877/sections_1-10.dart
@@ -67,7 +67,7 @@ class PersonSectionOne7877 extends StatelessWidget {
                           child: Column(
                             children: [
                               Text(
-                                'Section 1. Short Title.',
+                                'Section 1. Title.',
                                 style: TextStyle(
                                   fontFamily: "RobotoFlex",
                                   fontSize: 20.sp,
@@ -77,7 +77,7 @@ class PersonSectionOne7877 extends StatelessWidget {
                               ),
                               SizedBox(height: 20.h),
                               Text(
-                                '',
+                                'This Act shall be known as the "Anti-Sexual Harassment Act of 1995."',
                                 textAlign: TextAlign.justify,
                                 style: TextStyle(
                                   fontFamily: "RobotoFlex",
@@ -166,7 +166,7 @@ class PersonSectionTwo7877 extends StatelessWidget {
                           child: Column(
                             children: [
                               Text(
-                                'Section 2. Statement of Policy.',
+                                'Section 2. Declaration of Policy.',
                                 style: TextStyle(
                                   fontFamily: "RobotoFlex",
                                   fontSize: 20.sp,
@@ -176,7 +176,11 @@ class PersonSectionTwo7877 extends StatelessWidget {
                               ),
                               SizedBox(height: 20.h),
                               Text(
-                                '',
+                                'The State shall value the dignity of every individual, '
+                                'enhance the development of it human resources, guarantee full respect for human rights, and '
+                                'uphold the dignity of workers, employees, applicants for employment, students or those '
+                                'undergoing training, instruction or education. Towards this end, all forms of sexual harassment '
+                                'in the employment, education or training environment are hereby declared unlawful.\n',
                                 textAlign: TextAlign.justify,
                                 style: TextStyle(
                                   fontFamily: "RobotoFlex",
@@ -265,7 +269,8 @@ class PersonSectionThree7877 extends StatelessWidget {
                           child: Column(
                             children: [
                               Text(
-                                'Section 3. Definitions.',
+                                'Section 3. Work, Education or Training-related Sexual Harassment Defined.',
+                                textAlign: TextAlign.center,
                                 style: TextStyle(
                                   fontFamily: "RobotoFlex",
                                   fontSize: 20.sp,
@@ -275,7 +280,25 @@ class PersonSectionThree7877 extends StatelessWidget {
                               ),
                               SizedBox(height: 20.h),
                               Text(
-                                '',
+                                'Work, education or training-related sexual harassment is committed by an employee, manager, supervisor, agent '
+                                'of the employer, teacher, instructor, professor, coach, trainor, or any other person who, having '
+                                'authority, influence or moral ascendancy over another in a work or training or education '
+                                'environment, demands, requests or otherwise requires any sexual favor from the other, regardless '
+                                'of whether the demand, request or requirement for submission is accepted by the object of said Act.\n\n'
+                                '(a) In a work-related or employment environment, sexual harassment is committed when:\n'
+                                '(1) The sexual favor is made as a condition in the hiring or in the employment, re-'
+                                'employment or continued employment of said individual, or in granting said individual favorable '
+                                'compensation, terms, conditions, promotions, or privileges; or the refusal to grant the sexual '
+                                'favor results in limiting, segregating or classifying the employee which in a way would '
+                                'discriminate, deprive or diminish employment opportunities or otherwise adversely affect said employee;\n'
+                                "(2) The above acts would impair the employee's rights or privileges under existing labor laws; or\n"
+                                '(3) The above acts would result in an intimidating, hostile, or offensive environment for the employee.\n\n'
+                                '(b) In an education or training environment, sexual harassment is committed:\n'
+                                '(1) Against one who is under the care, custody or supervision of the offender;\n'
+                                '(2) Against one whose education, training, apprenticeship or tutorship is entrusted to the offender;\n'
+                                '(3) When the sexual favor is made a condition to the giving of a passing grade, or the '
+                                'granting of honors and scholarships, or the payment of a stipend, allowance or other benefits, privileges, or considerations; or\n'
+                                '(4) When the sexual advances result in an intimidating, hostile or offensive environment for the student, trainee or apprentice. Any person who directs or induces another to commit any act of sexual harassment as herein defined, or who cooperates in the commission thereof by another without which it would not have been committed, shall also be held liable under this Act.\n',
                                 textAlign: TextAlign.justify,
                                 style: TextStyle(
                                   fontFamily: "RobotoFlex",
@@ -364,7 +387,8 @@ class PersonSectionFour7877 extends StatelessWidget {
                           child: Column(
                             children: [
                               Text(
-                                'Section 4. Acts of Torture.',
+                                'Section 4. Duty of the Employer or Head of Office in a Work-related, Education or Training Environment.',
+                                textAlign: TextAlign.center,
                                 style: TextStyle(
                                   fontFamily: "RobotoFlex",
                                   fontSize: 20.sp,
@@ -374,7 +398,29 @@ class PersonSectionFour7877 extends StatelessWidget {
                               ),
                               SizedBox(height: 20.h),
                               Text(
-                                '',
+                                'It shall be the duty of the employer or the head of the work-related, educational '
+                                'or training environment or institution, to prevent or deter the commission of acts of sexual '
+                                'harassment and to provide the procedures for the resolution, settlement or prosecution of acts of '
+                                'sexual harassment. Towards this end, the employer or head of office shall:\n\n'
+                                '(a) Promulgate appropriate rules and regulations in consultation with the jointly '
+                                'approved by the employees or students or trainees, through their duly designated representatives, '
+                                'prescribing the procedure for the investigation or sexual harassment cases and the administrative '
+                                'sanctions therefor. Administrative sanctions shall not be a bar to prosecution in the proper courts for '
+                                'unlawful acts of sexual harassment. The said rules and regulations issued pursuant to this section '
+                                '(b) shall include, among others, guidelines on proper decorum in the workplace and educational or '
+                                'training institutions.\n\n'
+                                '(c) Create a committee on decorum and investigation of cases on sexual harassment. '
+                                'The committee shall conduct meetings, as the case may be, with other officers and employees, '
+                                'teachers, instructors, professors, coaches, trainors and students or trainees to increase '
+                                'understanding and prevent incidents of sexual harassment. It shall also conduct the investigation '
+                                'of the alleged cases constituting sexual harassment. In the case of a work-related environment, the committee shall be composed of at least one '
+                                '(1) representative each from the management, the union, if any, the employees from the '
+                                'supervisory rank, and from the rank and file employees. '
+                                'In the case of the educational or training institution, the committee shall be composed of at '
+                                'least one (1) representative from the administration, the trainers, teachers, instructors, professors '
+                                'or coaches and students or trainees, as the case maybe.\n'
+                                '"The employer or head of office, educational or training institution shall disseminate or post '
+                                'a copy of this Act for the information of all concerned.\n',
                                 textAlign: TextAlign.justify,
                                 style: TextStyle(
                                   fontFamily: "RobotoFlex",
@@ -463,7 +509,7 @@ class PersonSectionFive7877 extends StatelessWidget {
                           child: Column(
                             children: [
                               Text(
-                                'Section 5. Other Cruel, Inhuman and Degrading Treatment or Punishment.',
+                                'Section 5. Liability of the Employer, Head of Office, Educational or Training Institution.',
                                 textAlign: TextAlign.center,
                                 style: TextStyle(
                                   fontFamily: "RobotoFlex",
@@ -474,7 +520,10 @@ class PersonSectionFive7877 extends StatelessWidget {
                               ),
                               SizedBox(height: 20.h),
                               Text(
-                                '',
+                                'The employer or head of office, educational training institution shall be solidarily liable for damage '
+                                'arising from the acts of sexual harassment committed in the employment, education or training '
+                                'environment if the employer or head of office, educational or training institution is informed of '
+                                'such acts by the offended party and no immediate action is taken thereon.\n',
                                 textAlign: TextAlign.justify,
                                 style: TextStyle(
                                   fontFamily: "RobotoFlex",
@@ -563,7 +612,7 @@ class PersonSectionSix7877 extends StatelessWidget {
                           child: Column(
                             children: [
                               Text(
-                                'Section 6. Freedom from Torture and Other Cruel, Inhuman and Degrading Treatment or Punishment, An Absolute Bight',
+                                'Section 6. Independent Action for Damages.',
                                 textAlign: TextAlign.center,
                                 style: TextStyle(
                                   fontFamily: "RobotoFlex",
@@ -574,7 +623,9 @@ class PersonSectionSix7877 extends StatelessWidget {
                               ),
                               SizedBox(height: 20.h),
                               Text(
-                                '',
+                                'Nothing in this Act shall preclude the victim of '
+                                'work, education or training-related sexual harassment from instituting a separate and '
+                                'independent action for damages and other affirmative relief.\n',
                                 textAlign: TextAlign.justify,
                                 style: TextStyle(
                                   fontFamily: "RobotoFlex",
@@ -663,7 +714,7 @@ class PersonSectionSeven7877 extends StatelessWidget {
                           child: Column(
                             children: [
                               Text(
-                                'Section 7. Prohibited Detention.',
+                                'Section 7. Penalties.',
                                 style: TextStyle(
                                   fontFamily: "RobotoFlex",
                                   fontSize: 20.sp,
@@ -673,7 +724,11 @@ class PersonSectionSeven7877 extends StatelessWidget {
                               ),
                               SizedBox(height: 20.h),
                               Text(
-                                '',
+                                'Any person who violates the provisions of this Act shall, upon '
+                                'conviction, be penalized by imprisonment of not less than one (1) month nor more than six (6) '
+                                'months, or a fine of not less than Ten thousand pesos (P10,000) nor more than Twenty thousand '
+                                'pesos (P20,000), or both such fine and imprisonment at the discretion of the court. '
+                                'Any action arising from the violation of the provision of this Act shall prescribe in three (3) years.\n',
                                 textAlign: TextAlign.justify,
                                 style: TextStyle(
                                   fontFamily: "RobotoFlex",
@@ -762,7 +817,7 @@ class PersonSectionEight7877 extends StatelessWidget {
                           child: Column(
                             children: [
                               Text(
-                                'Section 8. Applicability of the Exclusionary Rule; Exception.',
+                                'Section 8. Separability Clause.',
                                 textAlign: TextAlign.center,
                                 style: TextStyle(
                                   fontFamily: "RobotoFlex",
@@ -773,7 +828,8 @@ class PersonSectionEight7877 extends StatelessWidget {
                               ),
                               SizedBox(height: 20.h),
                               Text(
-                                '',
+                                'If any portion or provision of this Act is declared void and '
+                                'unconstitutional, the remaining portions or provisions hereof shall not be affected by such declaration.',
                                 textAlign: TextAlign.justify,
                                 style: TextStyle(
                                   fontFamily: "RobotoFlex",
@@ -862,7 +918,7 @@ class PersonSectionNine7877 extends StatelessWidget {
                           child: Column(
                             children: [
                               Text(
-                                'Section 9. Institutional Protection of Torture Victims and other Persons Involved.',
+                                'Section 9. Repealing Clause.',
                                 style: TextStyle(
                                   fontFamily: "RobotoFlex",
                                   fontSize: 20.sp,
@@ -872,7 +928,8 @@ class PersonSectionNine7877 extends StatelessWidget {
                               ),
                               SizedBox(height: 20.h),
                               Text(
-                                '',
+                                'All laws, decrees, orders, rules and regulations, other issuances, '
+                                'or parts thereof inconsistent with the provisions of this Act are hereby repealed or modified accordingly.',
                                 textAlign: TextAlign.justify,
                                 style: TextStyle(
                                   fontFamily: "RobotoFlex",
@@ -961,7 +1018,7 @@ class PersonSectionTen7877 extends StatelessWidget {
                           child: Column(
                             children: [
                               Text(
-                                'Section 10. Disposition of Writs of Habeas Corpus, Amparo and Habeas Data Proceedings and Compliance with a Judicial Order.',
+                                'Section 10. Effectivity Clause.',
                                 textAlign: TextAlign.center,
                                 style: TextStyle(
                                   fontFamily: "RobotoFlex",
@@ -972,7 +1029,20 @@ class PersonSectionTen7877 extends StatelessWidget {
                               ),
                               SizedBox(height: 20.h),
                               Text(
-                                '',
+                                'This Act shall take effect fifteen (15) days after its complete '
+                                'publication in at least two (2) national newspaper of general circulation.\n\n'
+                                'Approved,\n'
+                                'EDGARDO J. ANGARA JOSE DE VENECIA, JR. '
+                                'President of Senate Speaker of the House '
+                                'of Representatives\n\n'
+                                'This Act which is a consolidation of House Bill NO. 9425 and Senate Bill 1632 was finally '
+                                'passed by the House of Representatives and the Senate on February 8, 1995.\n\n'
+                                'EDGARDO E. TUMANGAN CAMILO L. SABIO '
+                                'Secretary of the Senate Secretary General '
+                                'House of Representatives\n'
+                                'Approved : Feb 14, 1995\n\n'
+                                'FIDEL V. RAMOS\n'
+                                'President of the Philippines\n',
                                 textAlign: TextAlign.justify,
                                 style: TextStyle(
                                   fontFamily: "RobotoFlex",
